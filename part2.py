0ab4c958ee3944cd7418e4172cfcfd45fadf0c2f
Gwendal <gwendaaaaaaal@gmail.com>
2021-12-18 18:44:55 +0100

    1/3 part 2

--- part2.py
+++ part2.py
@@ -7,10 +7,11 @@ books_file = "./Ressources/books.txt"
 scoring_matrix_file = "Ressources/rating_matrix.txt"
 from part1 import *
 
-###  PART TWO PRIMARY FUNCTIONS
+#  PART TWO PRIMARY FUNCTIONS
 
 
-# This function allows to navigate through part2 functions
+# This function allows to navigate through part2 functions with the 1st "if" block
+# It also has a 2nd "if" statement to handle back and exit cases.
 def menu_part2():
     print("Write 1 to add a book")
     print("Write 2 to edit a book")
@@ -39,20 +40,22 @@ def menu_part2():
 
 # This function allows to add a book to "books.txt".
 def addBook():
+    # Open the files we will need to edit and get a list of their lines for each.
     list_of_books = open(books_file, "r")
     scoring_matrix = open(scoring_matrix_file, "r")
     matrix_lines = scoring_matrix.readlines()
     list_of_books_readlines = list_of_books.readlines()
+    # Receive
     book_to_append = input("What book do you want to add ?\n")
     while book_to_append in [x.replace('\n', '') for x in list_of_books_readlines]:
         print("The book is already in the list\n")
         book_to_append = input("What book do you want to add ? Write 'back' to go back.\n")
-        if book_to_append == "back":
-            return menu_part2()
     if book_to_append == "back":
         return menu_part2()
     elif book_to_append == "exit":
         return 0
+    # Removes the "\n" at the end of every lines of "rating_matrix.txt", then append a zero with "\n" at the end of each lines
+    # (which basically create a new column). We then appends the name of the new book at the end of "books.txt".
     for cpt in range(len(matrix_lines)):
         line = matrix_lines[cpt].rstrip()
         line += " 0\n"
@@ -71,6 +74,7 @@ def editBook():
     backbool = True
     displayBooks()
     book_to_edit = input("Which book do you want to edit ? Write 'back' to go back.\n")
+    # Check if the book indeed exists and manage "back" & "exit" inputs.
     while book_to_edit not in [x.replace('\n', '') for x in list_of_books_readlines] and book_to_edit != "back" and book_to_edit != "exit":
         print("The book is not in the list")
         book_to_edit = input("Which book do you want to edit ? Write 'back' to go back.\n")
